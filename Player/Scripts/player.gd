1308bcf24f5ef8a69bd20fd7aef71fa55bd89bf8
rcdavis <rcdavisgd@gmail.com>
2025-05-01 18:02:34 -0400

    Added grass tile map

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -11,8 +11,10 @@ func _ready():
 	state_machine.initialize(self)
 
 func _process(delta: float):
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	).normalized()
 
 func _physics_process(delta: float):
 	move_and_slide()
