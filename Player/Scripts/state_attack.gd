ad2794f0b8b6ac52ddf8c62b859563c28bea3d67
rcdavis <rcdavisgd@gmail.com>
2025-05-01 20:24:34 -0400

    Updated attack collisions

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -11,7 +11,7 @@ var is_attacking: bool = false
 
 @onready var idle: State = $"../Idle"
 @onready var walk: State = $"../Walk"
-@onready var hurt_box: HurtBox = $"../../Interactions/HurtBox"
+@onready var hurt_box: HurtBox = %AttackHurtBox
 
 func enter():
 	player.update_animation("attack")
