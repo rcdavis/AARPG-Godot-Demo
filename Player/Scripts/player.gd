598c7f228b631ed2ba741411482a4aea282dd546
rcdavis <rcdavisgd@gmail.com>
2025-05-01 20:05:26 -0400

    Fixed player direction movements

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -1,5 +1,7 @@
 class_name Player extends CharacterBody2D
 
+const DIR_4 = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
+
 var cardinal_direction = Vector2.DOWN
 var direction = Vector2.ZERO
 
@@ -25,15 +27,12 @@ func set_direction() -> bool:
 	if direction == Vector2.ZERO:
 		return false
 	
-	var new_dir: Vector2 = cardinal_direction
-	if direction.y == 0:
-		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
-	elif direction.x == 0:
-		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
-		
+	var direction_id = int(round((direction + cardinal_direction * 0.1).angle() / TAU * DIR_4.size()))
+	var new_dir = DIR_4[direction_id]
+	
 	if new_dir == cardinal_direction:
 		return false
-		
+	
 	cardinal_direction = new_dir
 	direction_changed.emit(new_dir)
 	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
